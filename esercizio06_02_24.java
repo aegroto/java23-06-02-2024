150194cff8fdc1bbc8c1abfcb9491a5776d94c99
Ttamer <mericomattia@gmail.com>
2024-02-06 15:36:49 +0100

    create an exercise

---
+++ esercizio06_02_24.java
@@ -0,0 +1,11 @@
+public class esercizio06_02_24 {
+    public static void main(String[]args) {
+        System.out.println ("Hello World!");
+    }
+
+    public static String[] returnString (String[] arr) {
+        String[] makeArr = new String[arr.length];
+
+        return makeArr;
+    }
+}
