3119dfbbf9141a2e52de356145dadfa20cf7953c
Worro <alessiodelledonne29@gmail.com>
2024-02-06 16:04:18 +0100

    Ho aggiunto la sequenza for  nel modulo

--- esercizio06_02_24.java
+++ esercizio06_02_24.java
@@ -1,11 +1,26 @@
+import java.util.Arrays;
+
 public class esercizio06_02_24 {
     public static void main(String[]args) {
         System.out.println ("Hello World!");
+
+        String[] cose = {"Ciao","Serpente","gatto","cane"};
+
+        String[] result = returnString (cose);
+
+        System.out.println(Arrays.toString(result));
     }
 
     public static String[] returnString (String[] arr) {
         String[] makeArr = new String[arr.length];
 
+        for (int i=0; i< arr.length; i++){
+            String element = arr[i];
+            if (element.length()%2 == 0) {
+                makeArr[i]=arr[i];
+            } else {
+                makeArr[i]=null;}
+        }
         return makeArr;
     }
 }
